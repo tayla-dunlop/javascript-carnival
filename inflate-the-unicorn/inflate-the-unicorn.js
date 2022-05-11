767a0f046edd5b4314c435b50c88dde1073a852b
Tayla Dunlop <tayladunlop@gmail.com>
2022-05-11 15:09:45 +1200

    fixed the unicorns

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,64 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+//change images in html
+//onclick method with a function to make it work everytime I click on a unicorn
+
+let unicorn = document.getElementsByClassName('inflate-an-image')
+
+for (let i = 0; i < unicorn.length; i++) {
+  unicorn[i].onclick = unicornClick
+}
+
+let balloonInflate = 0
+//make sure that when the unicorn has been clicked, it changes the image
+//do this by googling
+//maybe create an array for all the images
+function unicornClick(e) {
+  let unicorn = e.target
+  if (unicorn.id == 'unicorn-1') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('thanks')
+      alert('My horn is complete, Thank you!')
+    }
+  }
+
+  if (unicorn.id == 'unicorn-2') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('thanks')
+      alert('My horn is complete, Thank you!')
+    }
+  }
+
+  if (unicorn.id == 'unicorn-3') {
+    balloonInflate++
+
+    unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+    console.log(unicorn)
+
+    if (balloonInflate > 3) {
+      balloonInflate = 0
+      unicorn.src = './images/unicorn-' + balloonInflate + '.png'
+      console.log('thanks')
+      alert('My horn is complete, Thank you!')
+    }
+  }
+  console.log('You clicked on me!')
+}

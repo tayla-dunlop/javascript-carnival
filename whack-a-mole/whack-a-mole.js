737f2d9c8133c4016d65e737de43dcb0453e4dff
Tayla Dunlop <tayladunlop@gmail.com>
2022-05-12 16:07:39 +1200

    finished whack a mole challenge

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,38 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+//create a variable to store elements in cells
+
+let cells = document.getElementsByTagName('td')
+let moleImage = document.createElement('img')
+moleImage.classList.add('mole')
+moleImage.src = 'mole.PNG'
+moleImage.onclick = whackedMole
+//create a loop for it to go through
+
+//make a function that lets the mole pop up in any random cell
+//use math.random to make it random
+//source the mole image so a mole actually pops up
+function cellsClick() {
+  let randomIndex = Math.floor(Math.random() * 24)
+  randomCells = cells[randomIndex]
+
+  randomCells.appendChild(moleImage)
+}
+
+//call the function
+cellsClick()
+
+//make another function to make the mole disappear when clicked
+//fix bug of multiple moles in a single cell
+//fix other bug that allows you to click anywhere
+function whackedMole() {
+  console.log('Catch me if you can!')
+  cellsClick()
+  new Audio('whack-audio.wav').play()
+}
+
+//add sound effects!

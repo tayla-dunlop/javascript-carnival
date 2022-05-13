85350cb6fa4c6207256989d34e9d500dbc932470
Tayla Dunlop <tayladunlop@gmail.com>
2022-05-13 15:51:00 +1200

    best i can do

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,30 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+//create variable to change the heads everytime the function is called
+let headIndex = 0
+
+document.addEventListener('keydown', function (e) {
+  switch (e.key) {
+    case e.key:
+      changeClownHead()
+      break
+  }
+})
+
+//start by being able to change just 1 part of the body by making a function
+//give an id to the img tag in html
+//grab the image from html by using getElementById
+//change the source of the image so the clowns head is different
+//google how to add arow keys into javascript
+//add arrow keys!
+
+function changeClownHead() {
+  console.log(changeClownHead)
+  let head = document.getElementById('head')
+  head.src = './images/head1.png'
+  let headSrc = './images/head' + headIndex + '.png'
+}
